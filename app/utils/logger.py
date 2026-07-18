6973b2fb806009e3dc18d473ac1ed972090156ad
kingdevweb <kingbiznisonline@gmail.com>
2026-07-18 10:40:01 -0400

    feat: GitHub client, file processors, dashboard, deployment, web modules

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,1 +1,14 @@
-# TODO
+"""Structured logging."""
+import logging, sys
+from app.config import settings
+
+def setup_logger(name: str = "astra") -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(getattr(logging, settings.loglevel.upper(), logging.INFO))
+    if not logger.handlers:
+        h = logging.StreamHandler(sys.stdout)
+        h.setFormatter(logging.Formatter("%(asctime)s | %(levelname)-7s | %(name)s | %(message)s", "%Y-%m-%d %H:%M:%S"))
+        logger.addHandler(h)
+    return logger
+
+logger = setup_logger("astra")
